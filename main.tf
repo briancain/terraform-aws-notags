ce8dc3e4eb491362701fc89a2381736080018105
Brian Cain <bcain@hashicorp.com>
2020-07-20 11:33:39 -0700

    Bring in updates from helloworld repo

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
 resource "null_resource" "hello_world" {
   provisioner "local-exec" {
-    command = "echo hello worrrrrllldd!"
+    command = "echo hello world!!"
+  }
+}
+
+resource "null_resource" "hello_world_part_2" {
+  provisioner "local-exec" {
+    command = "echo ${var.greetings}!"
+  }
+}
+
+resource "null_resource" "dont_hack_me" {
+  provisioner "local-exec" {
+    command = "echo My password is: ${var.password}"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "greetings" {
+  type        = string
+  default     = "hello wooorrrlllddd this is a variable!!!"
+  description = "The default greetings message"
+}
+
+variable "password" {
+  type        = string
+  description = "My secret password don't hack me pls"
+}
